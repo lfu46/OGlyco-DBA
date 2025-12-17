c9e9ab5e16351bfea12d39a234391e943c6a40b4
Longping Fu <lpfu46@gatech.edu>
2025-12-16 20:48:37 -0500

    add data analysis for NGlyco result

---
+++ data_analysis/NGlyco_HCD_result.R
@@ -0,0 +1,62 @@
+# import packages
+library(tidyverse)
+
+# import NGlyco HCD searching result
+NGlyco_HCD_result <- read_tsv(
+  '/Volumes/cos-lab-rwu60/Longping/OGlyco_DBA_12152025/Standard_Extraction/NGlyco_HCD_Search_1/E_LF_OGlyco_DBA_1_12152025/psm.tsv',
+  col_names = TRUE,
+  name_repair = 'universal'
+) |> 
+  select(
+    Spectrum, Peptide, Charge, Observed.M.Z, Delta.Mass, Hyperscore, 
+    Assigned.Modifications, Total.Glycan.Composition, Glycan.Score, Glycan.q.value,
+    Is.Decoy, Is.Contaminant, Protein.ID, Entry.Name, Gene, Protein.Description
+  ) |> 
+  filter(
+    str_detect(Entry.Name, 'HUMAN')
+  ) |> 
+  filter(
+    Is.Decoy == FALSE & Is.Contaminant == FALSE
+  )
+
+# calculate specificity and the number of unique glycopeptide and glycoprotein
+Num_total_psm <- nrow(NGlyco_HCD_result)
+
+NGlyco_HCD_result_glycoPSM <- NGlyco_HCD_result |> 
+  filter(
+    !is.na(Total.Glycan.Composition)
+  )
+
+Num_glyco_psm <- nrow(NGlyco_HCD_result_glycoPSM)
+
+NGlyco_specificity <- Num_glyco_psm/Num_total_psm
+
+NGlyco_unique_glycopeptide <- NGlyco_HCD_result_glycoPSM |> 
+  select(Peptide, Total.Glycan.Composition) |> 
+  distinct()
+
+Num_unique_glycopeptide <- nrow(NGlyco_unique_glycopeptide)
+
+NGlyco_unique_glycoprotein <- NGlyco_HCD_result_glycoPSM |> 
+  distinct(Protein.ID)
+
+Num_unique_glycoprotein <- nrow(NGlyco_unique_glycoprotein)
+
+# apply glycan FDR filter with 0.05
+NGlyco_HCD_result_glycoPSM_FDR005 <- NGlyco_HCD_result_glycoPSM |> 
+  filter(Glycan.q.value <= 0.05)
+
+Num_glyco_psm_FDR005 <- nrow(NGlyco_HCD_result_glycoPSM_FDR005)
+
+NGlyco_unique_glycopeptide_FDR005 <- NGlyco_HCD_result_glycoPSM_FDR005 |> 
+  select(Peptide, Total.Glycan.Composition) |> 
+  distinct()
+
+Num_unique_glycopeptide_FDR005 <- nrow(NGlyco_unique_glycopeptide_FDR005)
+
+NGlyco_unique_glycoprotein_FDR005 <- NGlyco_HCD_result_glycoPSM_FDR005 |> 
+  distinct(Protein.ID)
+
+Num_unique_glycoprotein_FDR005 <- nrow(NGlyco_unique_glycoprotein_FDR005)
+
+
